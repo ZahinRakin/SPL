99604277eadf1c3c591dbde541e54dfb1e9bf15e
ZahinRakin <bsse1447@iit.du.ac.bd>
2023-12-17 01:59:46 +0600

    slight touch

--- socketMethods.c
+++ socketMethods.c
@@ -1,11 +1,11 @@
-#include <arpa/inet.h> // inet_addr()
+#include <arpa/inet.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h> // bzero()
+#include <strings.h>
 #include <sys/socket.h>
-#include <unistd.h> // read(), write(), close()
+#include <unistd.h>
 #define PORT 8080
 #define SA struct sockaddr
 
@@ -69,7 +69,6 @@ void download()
   int sockfd, connfd;
   struct sockaddr_in servaddr, cli;
 
-  // socket create and verification
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd == -1)
   {
@@ -80,12 +79,10 @@ void download()
     printf("Socket successfully created..\n");
   bzero(&servaddr, sizeof(servaddr));
 
-  // assign IP, PORT
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = inet_addr("192.168.0.104"); // this needs to be chagned.
   servaddr.sin_port = htons(PORT);
 
-  // connect the client socket to server socket
   if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) != 0)
   {
     printf("connection with the server failed...\n");
@@ -94,10 +91,8 @@ void download()
   else
     printf("connected to the server..\n");
 
-  // function for chat
   recvFile(sockfd);
 
-  // close the socket
   close(sockfd);
 }
 
@@ -132,7 +127,6 @@ void uploadToServer(char *username)
   int sockfd, connfd;
   struct sockaddr_in servaddr, cli;
 
-  // socket create and verification
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd == -1)
   {
@@ -143,12 +137,10 @@ void uploadToServer(char *username)
     printf("Socket successfully created..\n");
   bzero(&servaddr, sizeof(servaddr));
 
-  // assign IP, PORT
   servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = inet_addr("192.168.0.104");
+  servaddr.sin_addr.s_addr = inet_addr("192.168.0.104"); /////////
   servaddr.sin_port = htons(PORT);
 
-  // connect the client socket to server socket
   if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) != 0)
   {
     printf("connection with the server failed...\n");
@@ -157,9 +149,7 @@ void uploadToServer(char *username)
   else
     printf("connected to the server..\n");
 
-  // function for chat
   uploadFile(sockfd, username);
 
-  // close the socket
   close(sockfd);
 }
